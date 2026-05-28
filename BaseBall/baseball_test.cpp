a0d9033c873baa94e8c651d700f2f9412f68bdaf
donghyun.yoon <ydh1305@naver.com>
2026-05-28 13:38:57 +0900

    [refactoring] page #26

--- BaseBall/baseball_test.cpp
+++ BaseBall/baseball_test.cpp
@@ -17,20 +17,12 @@ public:
 	}
 };
 
-TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
-	Baseball game;
-	EXPECT_THROW(game.guess(string("12")), length_error);
+TEST_F(BaseballFixture, ThrowExceptionWhenInputLengthIsUnmatched) {
+	assertIllegalArgument("12");
 }
 
-TEST(BaseballGame, ThrowExceptionWhenInvalidChar) {
-	Baseball game;
-	try {
-		game.guess(string("12s"));
-		FAIL();
-	}
-	catch (exception e) {
-
-	}
+TEST_F(BaseballFixture, ThrowExceptionWhenInvalidChar) {
+	assertIllegalArgument("12s");
 }
 
 int main() {
