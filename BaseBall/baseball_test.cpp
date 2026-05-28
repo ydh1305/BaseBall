7142093ca6192006f4302f26f2c86e17908dc7bb
donghyun.yoon <ydh1305@naver.com>
2026-05-28 13:14:01 +0900

    [refactoring] page #23

--- BaseBall/baseball_test.cpp
+++ BaseBall/baseball_test.cpp
@@ -8,7 +8,13 @@ TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
 
 TEST(BaseballGame, ThrowExceptionWhenInvalidChar) {
 	Baseball game;
-	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+	try {
+		game.guess(string("12s"));
+		FAIL();
+	}
+	catch (exception e) {
+
+	}
 }
 
 int main() {
