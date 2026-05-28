9da5d2f1c1511117325312213c908fca013e8e7a
donghyun.yoon <ydh1305@naver.com>
2026-05-28 13:54:23 +0900

    [refactoring] page #42

--- BaseBall/baseball.cpp
+++ BaseBall/baseball.cpp
@@ -14,7 +14,9 @@ public:
 
 	GuessResult guess(const string& guessNumber) {
 		assertIllegalArgument(guessNumber);
-		return { true, 3, 0 };
+		if(guessNumber == question)
+			return { true, 3, 0 };
+		return { false, 0, 0 };
 	}
 	void assertIllegalArgument(const std::string& guessNumber)
 	{
